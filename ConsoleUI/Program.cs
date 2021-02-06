2d68895cbd02a3ff4168f63d3a03efd8147fcfef
Batuhan Arslan <bthnarsln64@gmail.com>
2021-02-06 12:14:19 +0300

    brandId ve colorId'ye göre veri çekme

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,11 +11,18 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             CarManager carManager = new CarManager(new EfCarDal());
-            foreach (var car in carManager.GetAll())
-            {
-                Console.WriteLine("Açıklama: " + car.Description + " " + car.ModelYear + " Model " + "- " + car.DailyPrice + "TL/Gün"); ;
-            }
-            carManager.Add(new Car { CarId = 3, BrandId = 2, ColorId = 3, DailyPrice = -3, ModelYear = "2017", Description = "sgasgf" });
+
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine("Açıklama: " + car.Description + " " + car.ModelYear + " Model " + "- " + car.DailyPrice + "TL/Gün"); ;
+            //}
+
+            // carManager.Add(new Car { CarId = 3, BrandId = 2, ColorId = 3, DailyPrice = 200, ModelYear = "2017", Description = "Ford Courier 2017 Model" });
+            carManager.Add(new Car { CarId = 4, BrandId = 3, ColorId = 3, DailyPrice = -3, ModelYear = "2017", Description = "Fiat Linea" });
+
+            //Console.WriteLine(carManager.GetCarsByBrandId(3).Description);
+            //Console.WriteLine(carManager.GetCarsByColorId(2).Description);
+
             BrandManager brandManager = new BrandManager(new EfBrandDal());
             brandManager.Add(new Brand { BrandId = 2, BrandName = "a"});
         }
